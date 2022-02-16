58e8efad6dea6e6256c53e4d20b6dbf654931db5
naoya7081111 <naoya7081111@gmail.com>
2022-02-16 10:54:09 +0900

    タスクカードの実装

--- src/conmponents/task/Tasks.jsx
+++ src/conmponents/task/Tasks.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Task } from './Task';
 
-export const Tasks = () => {
+export const Tasks = (props) => {
+
+  const { taskList, setTaskList } = props;
+
   return (
-    <div>Tasks</div>
+    <div>
+      {taskList.length !== 0 && taskList.map((task, index) => (
+        <div key={index}>
+          <Task task={task} taskList={taskList} setTaskList={setTaskList} />
+        </div>
+      ))}
+    </div>
   )
 }
