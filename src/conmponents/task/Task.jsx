c6c634b18c908678d67d0dad22c74242432c2f23
naoya7081111 <naoya7081111@gmail.com>
2022-02-17 11:05:02 +0900

    ドラッグドロップの実装

--- src/conmponents/task/Task.jsx
+++ src/conmponents/task/Task.jsx
@@ -3,20 +3,20 @@ import { Draggable } from 'react-beautiful-dnd';
 
 export const Task = (props) => {
 
-    const { task, taskList, setTaskList } = props;
+    const { task, taskList, setTaskList, index } = props;
     const handleDelete = (id) => {
         setTaskList(taskList.filter((task) => task.id !== id));
     }
 
     return (
-        <Draggable index={task.id} draggableId={task.draggableId}>
+        <Draggable index={index} draggableId={task.draggableId} key={index}>
             {(provided) => (
-            <div className='taskBox' key={task.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
-                <p className='taskText'>{task.text}</p>
-                <button className='taskTrashButton' onClick={() => handleDelete(task.id)}>
-                    <i className="fa-solid fa-trash-can"></i>
-                </button>
-            </div>
+                <div className='taskBox' key={task.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
+                    <p className='taskText'>{task.text}</p>
+                    <button className='taskTrashButton' onClick={() => handleDelete(task.id)}>
+                        <i className="fa-solid fa-trash-can"></i>
+                    </button>
+                </div>
             )}
         </Draggable>
     )
--- src/conmponents/task/Tasks.jsx
+++ src/conmponents/task/Tasks.jsx
@@ -6,16 +6,25 @@ export const Tasks = (props) => {
 
   const { taskList, setTaskList } = props;
 
+  const reorder = (taskList, startIndex, endIndex) => {
+    // タスクの並び替え
+    const remove = taskList.splice(startIndex, 1); //ドラッグ元を削除
+    taskList.splice(endIndex, 0, remove[0]); //ドラッグしたものを目的地に配置  
+  }
+
+  const handleDragEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
+    setTaskList(taskList);
+  }
+
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId='droppableId' >
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef} >
               {taskList.length !== 0 && taskList.map((task, index) => (
-                <div key={index}>
-                  <Task task={task} taskList={taskList} setTaskList={setTaskList} />
-                </div>
+                <Task index={index} task={task} taskList={taskList} setTaskList={setTaskList} key={task.id}/>
               ))}
               {provided.placeholder}
             </div>
