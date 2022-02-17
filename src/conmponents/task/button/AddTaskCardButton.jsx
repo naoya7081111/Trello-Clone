c3a8b400075fe3fd10c0e4d9b8720f98b59f13d4
naoya7081111 <naoya7081111@gmail.com>
2022-02-17 11:37:07 +0900

    タスクカード追加ボタンの実装

--- src/conmponents/task/button/AddTaskCardButton.jsx
+++ src/conmponents/task/button/AddTaskCardButton.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {v4 as uuid} from 'uuid';
 
-export const AddTaskCardButton = () => {
+export const AddTaskCardButton = (props) => {
+
+  const { taskCardsList, setTaskCardsList } = props;
+
+  const addTaskCard = () => {
+    const taskCardId = uuid();
+    setTaskCardsList([...taskCardsList, {
+      id: taskCardId,
+      draggableId: `item${taskCardId}`
+    }])
+  }
+
   return (
-    <div>AddTaskCardButton</div>
+    <div className='addTaskCardButtonArea'>
+      <button className='addTaskCardButton' onClick={addTaskCard} >+</button>
+    </div>
   )
 }
